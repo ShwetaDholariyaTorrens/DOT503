3d5e233e57e03881af856f9a8a8dd245092c8a54
ShwetaDholariyaTorrens <Shweta.Dholariya@Student.Torrens.edu.au>
2023-04-15 13:31:38 +0530

    Adding updated test cases with 3 correct and 2 wrong cases

--- app_test.py
+++ app_test.py
@@ -1,27 +1,21 @@
 import unittest
-from my_app import *
+from my_app import addition, subtraction, multiplication, division
 
 class TestMyApp(unittest.TestCase):
-    
-    # Successful Test Case 1: addition() function 
     def test_addition(self):
         self.assertEqual(addition(2, 3), 5)
-    
-    # Successful Test Case 2: subtraction() function
+
     def test_subtraction(self):
-        self.assertNotEqual(subtraction(5, 2), 2)
-        
-    # Failed Test Case 3: multiplication() function
+        self.assertNotEqual(subtraction(5, 3), 2)
+
     def test_multiplication(self):
-        self.assertTrue(multiplication(3, 4) == 11)
-        
-    # Failed Test Case 4: division() function
+        self.assertEqual(multiplication(3, 4) == 11)
+
     def test_division(self):
         self.assertEqual(division(10, 2), 6)
-        
-    # Successful Test Case 5: division() function 
+
     def test_division_by_zero(self):
-        with self.assertRaises(Exception): division(10, 0)
+        self.assertRaises(ZeroDivisionError, division, 10, 0)
 
 if __name__ == '__main__':
     unittest.main()
