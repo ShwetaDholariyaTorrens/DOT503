8b72dc2d7f960e322a1c4884ce1d8c28d3884817
ShwetaDholariyaTorrens <Shweta.Dholariya@Student.Torrens.edu.au>
2023-04-15 17:24:09 +0530

    Modified Code for testing

--- test.py
+++ test.py
@@ -6,7 +6,3 @@ def is_palindrome(word):
     return word == word[::-1]
 
 
-# test the is_palindrome function
-print(is_palindrome("racecar")) # True
-print(is_palindrome("hello"))   # False
-print(is_palindrome("A man a plan a canal Panama"))   # True
