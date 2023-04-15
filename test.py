b0ed94f0289eb8d7f67f1692ee0ea8685f7e2333
ShwetaDholariyaTorrens <Shweta.Dholariya@Student.Torrens.edu.au>
2023-04-15 17:16:47 +0530

    Modified Code for testing

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
-print("Hi, my name is Shweta")
-print("This is feature-z edit")
+def is_palindrome(word):
+    # convert the word to lowercase and remove any spaces
+    word = word.lower().replace(" ", "")
+  
+    # check if the word is equal to its reverse
+    return word == word[::-1]
+
+
+# test the is_palindrome function
+print(is_palindrome("racecar")) # True
+print(is_palindrome("hello"))   # False
+print(is_palindrome("A man a plan a canal Panama"))   # True
